baf6a8c2a8a5051f87cda625c167f87be15ee688
“manie9” <Fiixxssh@gmail.com>
2025-06-30 22:54:27 +0100

    Fresh push from week3-react-app

--- week3-react-app/vite.config.js
+++ week3-react-app/vite.config.js
@@ -4,7 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //base: '/week3-react-app/', // Set the base to your repository name
   plugins: [
     react(),
-    tailwindcss()],
+    tailwindcss(),
+  ],
 })
